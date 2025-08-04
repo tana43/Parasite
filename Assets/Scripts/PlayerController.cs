34013f9d9dfe3223a7a6366d3a128eb85f00effc
MSI\2220097 <tana61143@gmail.com>
2025-08-04 16:48:31 +0900

    基本の動きだけできるようになった 次は整理しつつ、設計を見直す

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,51 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    private Vector2 velocity_;
-
     [Header("移動速度"), SerializeField]
-    private float speed_ = 2.0f;
+    private float speed_ = 5.0f;
 
     [Header("最大落下速度"), SerializeField]
     private float maxFallSpeed_ = 10.0f;
 
-    [Header("重力"), SerializeField]
-    private float gravity_ = 9.8f;
-
     [Header("ジャンプの初速度"), SerializeField]
-    private float jumpSpeed_ = 3.0f;
+    private float jumpSpeed_ = 10.0f;
 
-    private CharacterController characterController_;
+    [Header("重力加速度"), SerializeField]
+    private float gravity_ = 9.8f;
+    
+
+    private Rigidbody2D rb_;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        characterController_ = GetComponent<CharacterController>();
+        rb_ = GetComponent<Rigidbody2D>();
+
+        // RigidBodyの当たり判定を座標基準から、移動した軌跡で計算するように変更
+        rb_.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        float ax = Input.GetAxis("Horizontal");
-        velocity_.x = ax * speed_;
+        MoveUpdate();
+    }
 
-        velocity_.y -= gravity_ * Time.deltaTime;
-        velocity_.y = Mathf.Max(velocity_.y, -maxFallSpeed_);
-
-        Vector3 move = new Vector3
-        (
-            velocity_.x * Time.deltaTime,
-            velocity_.y * Time.deltaTime,
-            0.00f
-        );
-        characterController_.Move(move);
-
-        if(characterController_.isGrounded)
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            velocity_.y = 0.0f;
+            Jump();
         }
+    }
+
+    private void MoveUpdate()
+    {
+        Vector2 velocity = rb_.velocity;
+
+        // 水平方向移動
+        float ax = Input.GetAxis("Horizontal");
+        if (Mathf.Approximately(ax, 0f))
+        {
+            velocity.x = 0f;
+        }
+        else
+        {
+            velocity.x = ax * speed_;
+        }
+
+        // 落下速度制限
+        velocity.y = Mathf.Max(velocity.y, -maxFallSpeed_);
+
+        rb_.velocity = velocity;
+
+        if(IsGrounded())
+        {
+            velocity.y = 0.0f;
+        }
+        
+    }
+
+    private void Jump()
+    {
+        // 一度、速度Yを０にして下降中でも地上と同じジャンプできるように
+        Vector2 velocity = rb_.velocity;
+        velocity.y = 0.0f;
+        rb_.velocity = velocity;
+        rb_.AddForce(Vector2.up * jumpSpeed_, ForceMode2D.Impulse);
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
+        return hit.collider != null;
     }
 }
