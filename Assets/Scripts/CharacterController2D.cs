868f9de6b15e2c0059c7dd51b7b8c849e88a70e2
DESKTOP-S2ELMGH\tana6 <tana61143@gmail.com>
2025-08-12 16:27:07 +0900

    移動処理をもう一度見直してる。

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -3,34 +3,57 @@ using System.Collections.Generic;
 using UnityEditor.Search;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
 public class CharacterController2D : MonoBehaviour
 {
-    private Rigidbody2D rb_;
-
-    [Header("移動速度"), SerializeField]
-    private float speed_ = 5.0f;
-
-    [Header("最大落下速度"), SerializeField]
-    private float maxFallSpeed_ = 10.0f;
-
-    [Header("ジャンプの初速度"), SerializeField]
-    private float jumpSpeed_ = 10.0f;
-
     [Header("地形判定となるレイヤー"),SerializeField] 
-    private LayerMask groundLayer;
+    private LayerMask groundLayer_;
 
-    private BoxCollider2D boxCollider2D_;
+    [Header("地上に立っているとみなす距離"), SerializeField]
+    private float groundedRaycastDistance_ = 0.1f;
+
+    Vector2 previousPosition_;
+    Vector2 currentPosition_;
+    Vector2 nextMovement_;
+
+    // 地上にいるか
+    public bool isGrounded_ { get; private set; }
+
+    // 天井に当たっているか
+    public bool isCeilinged_ { get; private set; }
+
+    public Vector2 velocity_ { get; private set; }
+
+    private Rigidbody2D rigidbody2D_;
+    private CapsuleCollider2D capsuleCollider2D_;
+    ContactFilter2D contactFilter_;
+    RaycastHit2D[] hitBuffer_ = new RaycastHit2D[5];
+    RaycastHit2D[] foundHits_ = new RaycastHit2D[3];
+    Collider2D[] groundColliders_ = new Collider2D[3];
+
 
     void Awake()
     {
-        rb_ = GetComponent<Rigidbody2D>();
-        boxCollider2D_ = GetComponent<BoxCollider2D>();
+        rigidbody2D_ = GetComponent<Rigidbody2D>();
+        capsuleCollider2D_ = GetComponent<CapsuleCollider2D>();
+
+        currentPosition_ = rigidbody2D_.position;
+        previousPosition_ = rigidbody2D_.position;
+
+        // raycastのオプション
+        contactFilter_.layerMask = groundLayer_;
+        contactFilter_.useLayerMask = true;
+        contactFilter_.useTriggers = false;
+
+        // Raycastの開始点が何かのコライダー内部からだったときにそのコライダーを無視するように設定
+        Physics2D.queriesStartInColliders = false;
     }
 
     private void Start()
     {
         // RigidBodyの当たり判定を座標基準から、移動した軌跡で計算するように変更
-        rb_.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        rigidbody2D_.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
     }
 
     // Update is called once per frame
@@ -45,47 +68,131 @@ public class CharacterController2D : MonoBehaviour
 
     private void MoveUpdate()
     {
-        Vector2 velocity = rb_.velocity;
-
-        // 落下速度制限
-        if (velocity.y < -maxFallSpeed_)
-        {
-            velocity.y = -maxFallSpeed_;
-        }
-
-        //if (IsGrounded())
-        //{
-        //    velocity.y = 0.0f;
-        //}
-
-        rb_.velocity = velocity;
-    }
-
-    public void Jump()
-    {
-        // 一度、速度Yを０にして下降中でも地上と同じジャンプできるように
-        Vector2 velocity = rb_.velocity;
-        velocity.y = 0.0f;
-        rb_.velocity = velocity;
-        rb_.AddForce(Vector2.up * jumpSpeed_, ForceMode2D.Impulse);
-    }
-
-    public bool IsGrounded()
-    {
-        float extraHeight = 0.1f;
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider2D_.bounds.center, boxCollider2D_.bounds.size, 0f, Vector2.down, extraHeight, groundLayer);
-        return hit.collider != null;
+        previousPosition_ = rigidbody2D_.position;
+        currentPosition_ = previousPosition_ + nextMovement_;
     }
 
     /// <summary>
-    /// 指定した方向に移動させる
-    /// 現状横方向のみの移動に限定している
+    /// Rigidbodyによる地形判定を持った移動処理。
     /// </summary>
-    /// <param name="moveDirectionX">移動する方向</param>
-    public void HorizontalMove(float moveDirectionX)
+    /// <param name="movement">移動値。単位はm/s</param>
+    public void Move(Vector2 movement)
     {
-        Vector2 velo = rb_.velocity;
-        velo.x = moveDirectionX * speed_;
-        rb_.velocity = velo;
+        nextMovement_ += movement;
+    }
+
+    /// <summary>
+    /// 当たり判定を無視した移動処理
+    /// </summary>
+    /// <param name="position">転移先の座標</param>
+    public void Teleport(Vector2 position)
+    {
+        Vector2 delta = position - currentPosition_;
+        previousPosition_ += delta;
+        currentPosition_ = position;
+        rigidbody2D_.MovePosition(position);
+    }
+
+    /// <summary>
+    /// これはisGroundedもしくはisCeilingedの状態を更新します。FixedUpdateでは自動的に呼び出されますが、より高い精度が必要な場合はより頻繁に呼び出すことができます。
+    /// </summary>
+    /// <param name="bottom"></param>
+    public void CheckCapsuleEndCollisions(bool bottom = true)
+    {
+        // レイを3本使用して地形判定を取る
+
+        Vector2 raycastDirection;
+        Vector2 raycastStart;
+        float raycastDistace;
+
+        if (capsuleCollider2D_ != null)
+        {
+            raycastStart = rigidbody2D_.position + capsuleCollider2D_.offset;
+            raycastDistace = capsuleCollider2D_.size.x * 0.5f + groundedRaycastDistance_ * 2f;
+
+            Vector2[] raycastPositions = new Vector2[3];
+
+            if (bottom)
+            {
+                raycastDirection = Vector2.down;
+                Vector2 raycastStartBottomCenter = raycastStart + Vector2.down * (capsuleCollider2D_.offset.y * 0.5f - capsuleCollider2D_.offset.x * 0.5f);
+
+                raycastPositions[0] = raycastStartBottomCenter + Vector2.left * capsuleCollider2D_.size.x * 0.5f;
+                raycastPositions[1] = raycastStartBottomCenter;
+                raycastPositions[2] = raycastStartBottomCenter + Vector2.right * capsuleCollider2D_.size.x * 0.5f;
+            }
+            else 
+            {
+                raycastDirection = Vector2.up;
+                Vector2 raycastStartBottomCenter = raycastStart + Vector2.up * (capsuleCollider2D_.offset.y * 0.5f + capsuleCollider2D_.offset.x * 0.5f);
+
+                raycastPositions[0] = raycastStartBottomCenter + Vector2.left * capsuleCollider2D_.size.x * 0.5f;
+                raycastPositions[1] = raycastStartBottomCenter;
+                raycastPositions[2] = raycastStartBottomCenter + Vector2.right * capsuleCollider2D_.size.x * 0.5f;
+            }
+
+            // 用意したレイの開始位置から下もしくは上へレイを飛ばす
+            for (int i = 0; i < raycastPositions.Length; i++)
+            {
+                int count = Physics2D.Raycast(raycastPositions[i], raycastDirection, contactFilter_, hitBuffer_, raycastDistace);
+
+                if (bottom)
+                {
+                    // 地面にあたった場合、そのコライダーとレイの情報を保持
+                    foundHits_[i] = count > 0 ? hitBuffer_[0] : new RaycastHit2D();
+                    groundColliders_[i] = foundHits_[i].collider;
+                }
+                else
+                {
+                    // 天井にあたったか
+
+                    isCeilinged_ = false;
+
+                    for (int j = 0; j < hitBuffer_.Length; j++)
+                    {
+                        if (hitBuffer_[j].collider != null)
+                        {
+                            isCeilinged_ = true;
+                        }
+                    }
+                }
+            }
+
+            if(bottom)
+            {
+                // レイが当たった地面の法線の平均値から地上にいているか判定する
+
+                Vector2 groundNormal = Vector2.zero;
+                int hitCount = 0;
+
+                for (int i = 0; i < foundHits_.Length; i++)
+                {
+                    if (foundHits_[i].collider != null)
+                    {
+                        groundNormal += foundHits_[i].normal;
+                        hitCount++;
+                    }
+                }
+
+                if(hitCount > 0)
+                {
+                    groundNormal.Normalize();
+                }
+
+                if (Mathf.Approximately(groundNormal.x, 0f) && Mathf.Approximately(groundNormal.y, 0f))
+                {
+                    isGrounded_ = false;
+                }
+                else
+                {
+                    isGrounded_ = velocity_.y <= 0f;
+                }
+            }
+
+            for (int i = 0; i < hitBuffer_.Length; i++)
+            {
+                hitBuffer_[i] = new RaycastHit2D();
+            }
+        }
     }
 }
